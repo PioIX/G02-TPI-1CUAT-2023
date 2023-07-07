84c670ac5a3e008458a1f1588f0be30d240ce0f5
ThiagooGasparini <129756771+ThiagooGasparini@users.noreply.github.com>
2023-07-07 17:33:39 -0300

    Update Base de datos.sql

--- Base de datos.sql
+++ Base de datos.sql
@@ -2,6 +2,7 @@ Create table usuarios	(
 	id_usuario int Primary key,
     nombre_usuario VARCHAR(50),
     contraseña_usuario VARCHAR(50),
+    dni_usuario VARCHAR(8),
    usuario_admin boolean);
    
    Create table puntajes (
@@ -10,8 +11,7 @@ Create table usuarios	(
     
     Create table palabras	(
 	id_palabra int Primary key,
-    nombre_palabra VARCHAR(50),
-    definicion_palabra VARCHAR(50));
+    nombre_palabra VARCHAR(50));
     
     
 Insert into usuarios (id_usuario, nombre_usuario, contraseña_usuario, usuario_admin) values (1,"chachi", "alpaca16", TRUE);
@@ -20,15 +20,17 @@ Insert into usuarios (id_usuario, nombre_usuario, contraseña_usuario, usuario_a
 Insert into usuarios (id_usuario, nombre_usuario, contraseña_usuario, usuario_admin) values (4,"lalo", "joroba45", TRUE);
  
 
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (1,"consumo", "es la acción y efecto de consumir o gastar productos, bienes o servicios");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (2,"energia", "alpaca16");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (3,"pobreza", "situación en la cual no es posible satisfacer las necesidades físicas y psicológicas básicas de una persona");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (4,"alianza", "alpaca16");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (5,"residuo", "alpaca16");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (6,"innovar", "alpaca16");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (7,"planeta", "alpaca16");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (8,"trabajo", "alpaca16");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (9,"generos", "alpaca16");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (10,"estados", "alpaca16");
-Insert into palabras (id_palabra, nombre_palabra, definicion_palabra) values (11,"derechos", "alpaca16");
+Insert into palabras (id_palabra, nombre_palabra) values (1,"consumo");
+Insert into palabras (id_palabra, nombre_palabra) values (2,"energia");
+Insert into palabras (id_palabra, nombre_palabra) values (3,"pobreza");
+Insert into palabras (id_palabra, nombre_palabra) values (4,"alianza");
+Insert into palabras (id_palabra, nombre_palabra) values (5,"residuo");
+Insert into palabras (id_palabra, nombre_palabra) values (6,"innovar");
+Insert into palabras (id_palabra, nombre_palabra) values (7,"planeta");
+Insert into palabras (id_palabra, nombre_palabra) values (8,"trabajo");
+Insert into palabras (id_palabra, nombre_palabra) values (9,"generos");
+Insert into palabras (id_palabra, nombre_palabra) values (10,"estados");
+Insert into palabras (id_palabra, nombre_palabra) values (11,"derechos");
+Insert into palabras (id_palabra, nombre_palabra) values (12,"sanidad");
+Insert into palabras (id_palabra, nombre_palabra) values (13,"jovenes");
 
