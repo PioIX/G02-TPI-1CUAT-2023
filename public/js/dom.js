778757ae510f2cd87ecc1fd2e48d5faa022e784a
samadey <129756395+santinoamadey@users.noreply.github.com>
2023-08-09 13:04:19 -0300

    MAS CAMBIOS
    Co-Authored-By: ThiagooGasparini <129756771+ThiagooGasparini@users.noreply.github.com>

--- public/js/dom.js
+++ public/js/dom.js
@@ -81,17 +81,52 @@ async function putJSON(data) {
 palabra = ["jovenes","sanidad","consumo","alianza","energia","derecho","estados","residuo"];
 
 function chequearPalabra(){
-  for (i in palabra[0]){
+  let palabraEscrita=""
+  let busqueda = palabra[0]
+  for (var i = palabra[0].length - 1; i >=palabra[0].length - 7 ; i--){
+    console.log(i)
+    palabraEscrita += i
     let caracter = document.getElementsByName("1")[i];
-    let busqueda = palabra[0]
-    if (busqueda[i]==caracter.value){
-      caracter.style.backgroundColor = "#008000";
-        } 
-    else if (busqueda[i]!=caracter.value && (palabra[0].includes(caracter.value))){
-      caracter.style.backgroundColor = "#f9e46e";
-      }
-    else{
+    caracter.style.backgroundColor = "#9b9b9b";
+    if (i>=7 && i<=13){
+      caracter = document.getElementsByName("2")[Math.floor(i/7)];
       caracter.style.backgroundColor = "#9b9b9b";
     }
+    else if (i>=14 && i<=20){
+      caracter = document.getElementsByName("3")[Math.floor(i/7)];
+      caracter.style.backgroundColor = "#9b9b9b";
+    }
+    else if (i>=21 && i<=27){
+      caracter = document.getElementsByName("4")[Math.floor(i/7)];
+      caracter.style.backgroundColor = "#9b9b9b";
+    }
+    else if (i>=28 && i<=34){
+       caracter = document.getElementsByName("5")[Math.floor(i/7)];
+       caracter.style.backgroundColor = "#9b9b9b";
+    }
+      if (busqueda[i]==caracter.value.toLowerCase() ){
+        caracter.style.backgroundColor = "#008000";
+          }
+      else if (caracter.value.length==0 ||palabra[0].includes(caracter.value)==false){
+        caracter.style.backgroundColofr = "#9b9b9b";
+          }
+      else if (busqueda[i]!=caracter.value && palabra[0].includes(caracter.value)==true ){
+        caracter.style.backgroundColor = "#f9e46e";
+          }
+    }
+    for (var x=0;x<7;x++){
+      palabra[0] += palabra[0][x]
+      console.log(palabra[0][x])
     }
 }
+
+
+function invertirCadena() {
+  var nuevaCadena = "";
+  for (var i = palabra[0].length - 1; i>=palabra[0].length - 7; i--) {
+      nuevaCadena += palabra[0][i];
+      console.log(palabra[0][i])
+  }
+  return nuevaCadena;
+}
+
