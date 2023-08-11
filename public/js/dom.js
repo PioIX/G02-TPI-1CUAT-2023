28b9839a8e7759b9b7747650427b8e84813e33d0
samadey <129756395+santinoamadey@users.noreply.github.com>
2023-08-11 08:46:03 -0300

    slice-agregado
    dom casi terminado
    
    Co-Authored-By: santinoamadey <129756395+santinoamadey@users.noreply.github.com>

--- public/js/dom.js
+++ public/js/dom.js
@@ -88,31 +88,24 @@ function chequearPalabra(){
     palabraEscrita += i
     let caracter = document.getElementsByName("1")[i];
     caracter.style.backgroundColor = "#9b9b9b";
-    if (i>=7 && i<=13){
-      caracter = document.getElementsByName("2")[Math.floor(i/7)];
-      caracter.style.backgroundColor = "#9b9b9b";
-    }
-    else if (i>=14 && i<=20){
-      caracter = document.getElementsByName("3")[Math.floor(i/7)];
-      caracter.style.backgroundColor = "#9b9b9b";
-    }
-    else if (i>=21 && i<=27){
-      caracter = document.getElementsByName("4")[Math.floor(i/7)];
-      caracter.style.backgroundColor = "#9b9b9b";
-    }
-    else if (i>=28 && i<=34){
-       caracter = document.getElementsByName("5")[Math.floor(i/7)];
-       caracter.style.backgroundColor = "#9b9b9b";
-    }
-      if (busqueda[i]==caracter.value.toLowerCase() ){
-        caracter.style.backgroundColor = "#008000";
-          }
-      else if (caracter.value.length==0 ||palabra[0].includes(caracter.value)==false){
-        caracter.style.backgroundColofr = "#9b9b9b";
-          }
-      else if (busqueda[i]!=caracter.value && palabra[0].includes(caracter.value)==true ){
-        caracter.style.backgroundColor = "#f9e46e";
-          }
+    if (busqueda[i]==caracter.value.toLowerCase() ){
+      caracter.style.backgroundColor = "#008000";
+      busqueda = busqueda.slice(0,i) + "-1" + busqueda.slice(i+1,busqueda.length) 
+      console.log(busqueda)
+      console.log(busqueda[i])
+        }
+  }
+  console.log(busqueda)
+  for (var i = palabra[0].length - 1; i >=palabra[0].length - 7 ; i--){
+    let caracter = document.getElementsByName("1")[i];
+    
+    if (caracter.value.length==0 ||busqueda.includes(caracter.value)==false){
+      caracter.style.backgroundColofr = "#9b9b9b";
+        }
+    else if (busqueda[i]!=caracter.value && busqueda.includes(caracter.value)==true ){
+      caracter.style.backgroundColor = "#f9e46e";
+      busqueda[i] = -1;
+        }
     }
     for (var x=0;x<7;x++){
       palabra[0] += palabra[0][x]
@@ -120,13 +113,6 @@ function chequearPalabra(){
     }
 }
 
-
-function invertirCadena() {
-  var nuevaCadena = "";
-  for (var i = palabra[0].length - 1; i>=palabra[0].length - 7; i--) {
-      nuevaCadena += palabra[0][i];
-      console.log(palabra[0][i])
-  }
-  return nuevaCadena;
+function comprobarVictoria(){
+  
 }
-
